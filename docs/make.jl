f3f9b31505634d6af58cf069587fb2914ffa6e3c
Fredrik Ekre <ekrefredrik@gmail.com>
2019-05-21 14:25:30 +0200

    Illustrate issue

--- docs/make.jl
+++ docs/make.jl
@@ -1,11 +1,1 @@
-using Documenter
-using LiterateLive
 using Literate
-
-Literate.markdown(joinpath(@__DIR__, "src", "index.jl"), joinpath(@__DIR__, "src"))
-
-makedocs(
-    sitename = "LiterateLive",
-    modules = [LiterateLive],
-    pages = ["Home" => "index.md"]
-)
--- src/LiterateLive.jl
+++ src/LiterateLive.jl
@@ -2,4 +2,8 @@ module LiterateLive
 
 greet() = print("Hello World!")
 
+function foo(x=include)
+    return parentmodule(x)
+end
+
 end # module
